77a51755cd029188f28bdd06132b074b47c7d309
ShuangYi <lishuangyi@exexpo.com.cn>
2018-12-18 10:55:35 +0800

    Initial Commit

---
+++ EyexpoVrSDKTests/EyexpoVrSDKTests.m
@@ -0,0 +1,37 @@
+//
+//  EyexpoVrSDKTests.m
+//  EyexpoVrSDKTests
+//
+//  Created by ShuangYi on 2018/12/18.
+//  Copyright © 2018 Eyexpo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EyexpoVrSDKTests : XCTestCase
+
+@end
+
+@implementation EyexpoVrSDKTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
